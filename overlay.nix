83b436131951317d988c5be31b4303db84bbc4b8
Ben Gamari <ben@smart-cactus.org>
2017-12-10 15:25:47 -0500

    Update knownGoodCrossed

--- overlay.nix
+++ overlay.nix
@@ -10,32 +10,27 @@ let
   };
 
   this = rec {
+    knownGoodCrossed = {
+      inherit (self) coreutils iproute procps curl
+        bashInteractive runit openssh openssl pcre2
+        diffutils ethtool file gawk gnupatch gnugrep pv
+        time htop strace netcat libusb eject watch
+        mmc-utils nettools sysfsutils iw asciidoc
+        sysstat libyaml nfs-utils gcc vim wget rsync
+        squashfsTools xz gnutar gzip gnumake which
+        nixUnstable python3 gdb autoconf automake
+        less nano procps-ng tmux i2c-tools cryptsetup
+        git ruby bridge-utils;
+    };
 
-  knownGoodCrossed = {
-    inherit (super) coreutils iproute procps curl
-      bashInteractive runit openssh openssl pcre2
-      diffutils ethtool file gawk gnupatch gnugrep pv
-      time htop strace netcat libusb eject watch
-      mmc-utils nettools sysfsutils iw asciidoc #bridge-utils
-      sysstat libyaml nfs-utils gcc
-      ;
-    # cryptsetup
-    # bridge-utils
-    # nfs-utils
-    # iotop: python dep
-    # emacs
-    # wget: ruby dependency
-    # wpa_supplicant
-    # sudo
-    # gcc
-    # systemd: Due to libapparmor
+    # Minimal git; namely omit tcl/tk and perl
+    git = super.git.override { guiSupport = false; perlSupport = false; };
+
+    # guile 2.2 doesn't cross compile. See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=28920
+    guile = super.guile_2_0;
+
+    # We really don't need libapparmor and it is a pain to cross compile due to
+    # its perl bindings
+    systemd = super.systemd.override { libapparmor = null; };
   };
-
-  # guile 2.2 doesn't cross compile. See https://debbugs.gnu.org/cgi/bugreport.cgi?bug=28920
-  guile = super.guile_2_0;
-
-  # We really don't need libapparmor and it is a pain to cross compile due to
-  # its perl bindings
-  systemd = super.systemd.override { libapparmor = null; };
-};
 in this
