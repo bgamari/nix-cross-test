23f91d4aa401cf0b78de172ac31add4a4e0c8299
Ben Gamari <ben@smart-cactus.org>
2018-04-23 10:28:05 -0400

    Rebase and cleanup

--- default.nix
+++ default.nix
@@ -1,21 +1,54 @@
-{nix ? (import ./nixpkgs)}:
+# Usage: nix-build ./nixos.nix -A system
 
 let
-  nixpkgs = nix {};
-  crossSystem = nixpkgs.lib.systems.examples.armv7l-hf-multiplatform // {
-    config = "armv7l-unknown-linux-gnueabihf";
-    #arch = "armv7-a";
-    #libc = "glibc";
-    withTLS = true;
-    openssl.system = "linux-armv4";
-    platform = nixpkgsCross.pkgs.platforms.armv7l-hf-multiplatform // {
-      kernelBaseConfig = "defconfig";
+  config_module = {config, lib, pkgs, ... }: {
+    environment.systemPackages = with pkgs; [
+      perlXMLParser
+      haskellPackages.heaps
+    ];
+    nixpkgs.crossSystem = {
+      config = "armv7l-unknown-linux-gnueabihf";
+      arch = "armv7l";
+      float = "hard";
+      fpu = "vfpv3-d16";
+      withTLS = true;
+      openssl.system = "linux-armv4";
+      platform = pkgs.platforms.armv7l-hf-multiplatform // {
+        kernelBaseConfig = "xilinx_zynq_defconfig";
+        kernelTarget = "uImage";
+        kernelMakeFlags = [ "LOADADDR=0x0200000" ];
+        uboot = pkgs.ubootMicrozed;
+      };
+    };
+
+    boot.loader.grub.enable = false;
+    boot.loader.generic-extlinux-compatible.enable = true;
+    boot.kernelPackages = pkgs.linuxPackagesFor pkgs.linux;
+    boot.kernelParams = [];
+    boot.supportedFilesystems = [ "vfat" ];
+  };
+
+  nixos = import ./nixpkgs/nixos {
+    configuration = {
+      imports = [ config_module ];
+
+      fonts.fontconfig.enable = false;
+      security.polkit.enable = false;
+      security.rngd.enable = false;
+      system.autoUpgrade.enable = false;
+      services.udisks2.enable = false;
+      services.nixosManual.enable = false;
+      services.bind.enable = false;
+
+      #nixpkgs.overlays = [ (import ./overlay) ];
+      nixpkgs.config.allowBroken = true;
+
+      fileSystems."/".device = "/dev/mmcblk0p2";
+
+      # FIXME: this probably should be in installation-device.nix
+      users.extraUsers.root.initialHashedPassword = "";
     };
   };
 
-nixpkgsCross = nix {
-  overlays = [(import ./overlay.nix)];
-  crossSystem = crossSystem;
-#  config = { defaultLd = "gold"; };
-};
-in nixpkgsCross
+in
+  nixos
